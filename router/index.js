fbbd393832696742396463780461b63798f95d36
Xavimat <xaviermatoses@gmail.com>
2022-05-16 15:52:02 +0200

    Add error 404 page and / as /home

--- router/index.js
+++ router/index.js
@@ -1,19 +1,23 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
-const PAGES = ['/home', '/about', '/what', '/where'];
+const PAGES = ['/', '/home', '/about', '/what', '/where'];
 
 
 http.createServer((req, res) => {
-    const { pathname:path } = url.parse(req.url, true);
+    let { pathname:path } = url.parse(req.url, true);
     let file = '404';
     let contentType;
     if (PAGES.includes(path)) {
+        path = path === '/' ? '/home' : path;
         file = `./pages/${path}.html`;
         contentType = { "Content-Type": "text/html" };
     } else if(path === '/css') {
         file = './css/bootstrap.min.css';
         contentType = { "Content-Type": "text/css" };
+    } else {
+        file = `./pages/404.html`;
+        contentType = { "Content-Type": "text/html" };
     }
     console.log(file);
     fs.readFile(file, (err, data) => {
