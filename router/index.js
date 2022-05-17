9f2463aea9f7f5cf6a17d619d8fd110a803ce769
Xavimat <xaviermatoses@gmail.com>
2022-05-17 10:28:04 +0200

    Separate html blocks

--- router/index.js
+++ router/index.js
@@ -3,10 +3,14 @@ const url = require("url");
 const fs = require("fs");
 const PAGES = ['/', '/home', '/about', '/what', '/where'];
 
+let header;
+fs.readFile('./pages/header.html', (err, data) => {header = data;});
 
 http.createServer((req, res) => {
+    const myCookies = req.headers.cookie?.toString();
     let { pathname:path } = url.parse(req.url, true);
     let file = '404';
+    let status = 200;
     let contentType;
     if (PAGES.includes(path)) {
         path = path === '/' ? '/home' : path;
@@ -18,16 +22,21 @@ http.createServer((req, res) => {
     } else {
         file = `./pages/404.html`;
         contentType = { "Content-Type": "text/html" };
+        status = 404;
     }
-    console.log(file);
+    console.log('Path',file);
     fs.readFile(file, (err, data) => {
         try {
-            res.writeHead(200, contentType);
-            res.write(data);
+            res.writeHead(status, contentType);
+            let content;
+            content = contentType["Content-Type"] === "text/html" ?
+                header + data + `Cookies: ${myCookies}.` :
+                data;
+            res.write(content);
             return res.end();
-        } catch (error) {
+        } catch (err) {
             res.writeHead(500, { "Content-Type": "text/html" });
-            console.error(error);
+            console.error(err);
             return res.end("Something went wrong");
         }
     })
